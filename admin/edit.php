a94480a75e69dff35e0cea1978d40d8813de8008
Clément Castel <36570169+Th3Sword290@users.noreply.github.com>
2018-04-27 20:11:13 +0200

    delete function
    Possibilty to erase data from the database

--- admin/edit.php
+++ admin/edit.php
@@ -12,17 +12,15 @@
 		}
 	}
 
-	if ( empty ( $post ) && empty ( $cat ) ) {
+	if ( empty ( $post ) ) {
 		$posts_array = $query->all_posts();
 	} elseif ( !empty ( $post ) ) {
 		$posts_array = $query->post($post);
-	} elseif ( !empty ( $cat ) ) {
-		echo 'cat';
 	}
 
 
-
-
+if (!empty($_GET['edit'])){
+if ($_GET['edit'] == 1){
 if (!empty($_POST)){
   $data[0] = addslashes($_POST['post_title']);
   $data[1] = addslashes($_POST['post_date']);
@@ -46,8 +44,22 @@ if (!empty($_POST)){
 	echo "Record updated successfully";
 } else {
 	echo "Error updating record: " . $conn->error;
+}}}}
+
+if (!empty($_GET['delete']) && !empty($_GET['id'])){
+
+		$query = "DELETE FROM posts
+		 				 WHERE ID = $id
+		";
+
+		if ($conn->query($query) === TRUE) {
+				echo "Suppression réussie";
+		} else {
+			echo "raté" . $conn->error;
+		}
+
 }
-  }
+
 ?>
 
 <html>
@@ -60,10 +72,10 @@ if (!empty($_POST)){
     <div class="list">
       <?php foreach ( $posts_array as $post ) : ?>
   					<a href="../article.php?p=<?php echo $post->ID; ?>">
-  					<p class="name"><?php echo $post->post_title ?></a> || <a href="./edit.php?id=<?php echo $post->ID; ?>&edit=1" >EDIT</a> || <a href="./delete.php?id=<?php echo $post->ID; ?>"> DELETE </a></p>
+  					<p class="name"><?php echo $post->post_title ?></a> || <a href="./edit.php?id=<?php echo $post->ID; ?>&edit=1" >EDIT</a> || <a href="./edit.php?id=<?php echo $post->ID; ?>&edit=2"> DELETE </a></p>
 
             <?php if (!empty($_GET['edit']) && $_GET['edit'] == 1) { ?>
-                <form method="post" enctype="multipart/form-data" action="edit.php?id=<?php echo $post->ID?>">
+                <form method="post" enctype="multipart/form-data" action="edit.php?id=<?php echo $post->ID?>&edit=1">
                     <input type="text" name="post_title" value="<?php echo $post->post_title; ?>" required /><br /><br />
                     <input type="text" name="post_date" value="<?php echo $post->post_date; ?>" required /><br /><br />
                     <input type="text" name="post_real" value="<?php echo $post->post_real; ?>" required /><br /><br />
@@ -77,7 +89,14 @@ if (!empty($_POST)){
                     <input type="text" name="post_ba" value="<?php echo $post->post_ba; ?>" required /><br /><br />
                     <input type="submit" value="Modifier"/>
                 </form>
-            <?php }  ?>
+            <?php } else if (!empty($_GET['edit']) && $_GET['edit'] == 2){ ?>
+
+								<h2> Voulez vous vraiment supprimer l'article: </h2>
+								<h4>	<a href="../article.php?p=<?php echo $post->ID; ?>"><?php echo $post->post_title; ?></a>  ?</h4>
+
+								<br /><button><a href="./edit.php?id=<?php echo $post->ID; ?>&delete=1">Supprimer</a></button>
+
+					<?php }?>
 
   		<?php endforeach; ?>
     </div>
