dc891de2efc63c9da2d54d6eefbb6890938b5417
Clément Castel <clement.castel75@gmail.com>
2018-07-06 14:06:39 +0200

    Restarting from scratch + add.php
    + Add.php file (integration + prog)

--- admin/add.php
+++ admin/add.php
@@ -1,139 +1,182 @@
-	<?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
+<?php
+require_once('../config.php');
+$conn = mysqli_connect(host, user,pass, db); //connection à la base de données
+print_r($_POST);
 
-	if ( !empty ( $_POST ) ) {
-		require_once('../includes/class-insert.php');
+if (isset($_POST['submit'])){ //vérification que le formulaire a bien été envoyé
 
-		if ( $insert->post($_POST) ) {
-			echo '<p>Data inserted successfully!</p>';
-		}
-	}
+$title = addslashes($_POST['title']);
+$date = addslashes($_POST['date']);
+$real = addslashes($_POST['real']);
+$actors = addslashes($_POST['actors']);
+$duration = addslashes($_POST['duration']);
+$languages = addslashes($_POST['languages']);
+$subs = addslashes($_POST['subs']);
+$quality = addslashes($_POST['quality']);
+$synopsis = addslashes($_POST['synopsis']);
+$trailer = addslashes($_POST['trailer']);
+$poster = addslashes($_POST['poster']);
+
+
+
+  $query = "INSERT INTO
+  `movies` (`title`, `date`, `film_director`, `actors`, `duration`, `languages`, `subs`, `quality`, `synopsis`, `ytb`, `poster`)
+VALUES
+  (
+    '$title',
+    '$date',
+    '$real',
+    '$actors',
+    '$duration',
+    '$languages',
+    '$subs',
+    '$quality',
+    '$synopsis',
+    '$trailer',
+    '$poster'
+  )"; //création de la query avec récupération des valeurs du formulaire
+
+  $insert = mysqli_query($conn,$query); //éxecution de la query
+
+  if ($insert) {
+    echo 'Article publié !'; //Si la query a été éxecutée correctement alors on affiche un message de confirmation
+  } else {
+    echo 'Il y a eu une erreur !'; //Dans le cas contraire, un message d'alerte
+    var_dump($conn->error); //accompagné de l'erreur.
+  }
+}
 ?>
 
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <title>Ajouter un film</title>
-  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.css'>
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'>
-  <link rel="stylesheet" href="./assets/css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AddAMovie</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/slate/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aldrich">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alfa+Slab+One">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anonymous+Pro">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/slate/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
 </head>
+
 <body>
+    <div class="login-dark" style="height:1950px;background-image:url(&quot;assets/img/bg-crop2.jpg&quot;);">
+        <form method="post" style="width:500px;">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-film-outline" style="color:rgb(255,255,255);"></i></div>
+            <div class="form-group">
+<input class="form-control" type="text" name="title" placeholder="IMDb Link" id="getData_url" style="font-family:'Alfa Slab One', cursive;">
+<button onclick="getData();" class="btn btn-dark active" type="button" style="padding-top:5px;padding-bottom:5px;padding-right:20px;padding-left:20px;margin-top:0px;width:370px;margin-bottom:70px;color:rgb(122,130,136);font-size:15px;font-family:'Alfa Slab One', cursive;">Pré-remplir les champs</button>
 
-  <div class="form">
-    <!-- - PANNEAU GAUCHE - -->
-  <div class="info">
-    <h2>Ajouter un film</h2>
-    <i class="icon ion-ios-film-outline" aria-hidden="true"></i>
+
+<input class="form-control" type="text" name="title" placeholder="Titre" id="title" style="font-family:'Alfa Slab One', cursive;">
+<input class="form-control" type="text" name="date" placeholder="Date" id="date" style="font-family:'Alfa Slab One', cursive;">
+<input class="form-control" type="text" name="real" placeholder="Réalisateur" id="real" style="font-family:'Alfa Slab One', cursive;">
+<input class="form-control" type="text" name="actors" placeholder="Acteurs" id="actors" style="font-family:'Alfa Slab One', cursive;">
+<input class="form-control" type="text" name="duration" placeholder="Durée" id="duration" style="font-family:'Alfa Slab One', cursive;">
+
+
+                        <!--<p style="font-family:'Alfa Slab One', cursive;padding-top:13px;padding-left:16px;color:rgb(122,130,136);">Genre :</p>
+                        <div class="custom-control custom-checkbox" style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox1" class="custom-control-input"><label class="custom-control-label" for="checkbox1" style="font-family:'Alfa Slab One', cursive;">Animation</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox2" class="custom-control-input"><label class="custom-control-label" for="checkbox2" style="font-family:'Alfa Slab One', cursive;">Fantastique / S-F</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox3" class="custom-control-input"><label class="custom-control-label" for="checkbox3" style="font-family:'Alfa Slab One', cursive;">Biopic</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox4" class="custom-control-input"><label class="custom-control-label" for="checkbox4" style="font-family:'Alfa Slab One', cursive;">Horreur</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox5" class="custom-control-input"><label class="custom-control-label" for="checkbox5" style="font-family:'Alfa Slab One', cursive;">Comédie</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox6" class="custom-control-input"><label class="custom-control-label" for="checkbox6" style="font-family:'Alfa Slab One', cursive;">Historique</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox7" class="custom-control-input"><label class="custom-control-label" for="checkbox7" style="font-family:'Alfa Slab One', cursive;">Documentaire</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox8" class="custom-control-input"><label class="custom-control-label" for="checkbox8" style="font-family:'Alfa Slab One', cursive;">Police / Thriller</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox9" class="custom-control-input"><label class="custom-control-label" for="checkbox9" style="font-family:'Alfa Slab One', cursive;">Drame</label></div>
+                        <div class="custom-control custom-checkbox"
+                            style="/*padding-left:0;*/color:rgb(122,130,136);"><input type="checkbox" id="checkbox10" class="custom-control-input"><label class="custom-control-label" for="checkbox10" style="font-family:'Alfa Slab One', cursive;">Aventure</label></div>
+
+-->
+<input class="form-control" type="text" name="languages" placeholder="Langues" style="font-family:'Alfa Slab One', cursive;margin-top:20px;">
+<input class="form-control" type="text" name="subs" placeholder="Sous-Titres" style="font-family:'Alfa Slab One', cursive;margin-bottom:20px;">
+
+                        <p style="color:rgb(119,130,136);font-family:'Alfa Slab One', cursive;padding-top:13px;margin-bottom:-12px;padding-left:16px;">Qualité :</p>
+            </div>
+            <div class="form-group">
+                <div class="custom-control custom-radio" style="margin-top:25px;"><input type="radio" id="radio1" class="custom-control-input" name="quality" value="-720p"><label class="custom-control-label" for="radio1" style="font-family:'Alfa Slab One', cursive;color:rgb(122,130,136);">-720p</label></div>
+                <div class="custom-control custom-radio"><input type="radio" id="radio2" class="custom-control-input" name="quality" value="720p"><label class="custom-control-label" for="radio2" style="font-family:'Alfa Slab One', cursive;color:rgb(122,130,136);">720p</label></div>
+                <div class="custom-control custom-radio"><input type="radio" id="radio3" class="custom-control-input" name="quality" value="1080p"><label class="custom-control-label" for="radio3" style="font-family:'Alfa Slab One', cursive;color:rgb(122,130,136);">1080p</label></div>
+                <p style="color:rgb(119,130,136);font-family:'Alfa Slab One', cursive;padding-top:35px;margin-bottom:0px;padding-left:16px;">Synopsis :</p>
+                <div class="form-group">
+
+
+<textarea class="form-control form-control" id="synopsis" name="synopsis" rows="3" style="height:330px;width:350px;margin-left:8px;background-color:#232e3b;margin-top:18px;"></textarea></div>
+<input class="form-control" type="text" name="trailer" placeholder="YTB Trailer link" id="ytb" style="font-family:'Alfa Slab One', cursive;padding-top:35px;margin-bottom:20px;">
+<input class="form-control" type="text" name="poster" placeholder="Poster link" id="poster" style="font-family:'Alfa Slab One', cursive;margin:0;"></div>
+
+
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit" value="submit" style="margin-top:100px;font-family:'Alfa Slab One', cursive;background-color:rgb(35,46,59);">Valider</button></div>
+    </form>
+    <p><a href="https://kelou.fr/">©KELOU </a>- CASTEL Clément</p>
   </div>
-    <!-- - PANNEAU DROITE (formulaire) - -->
-  <form method="post" class="addF" enctype="multipart/form-data">
-    <h2>Ajouter un film</h2>
-    <ul class="noBullet">
-      <!-- - TITRE - -->
-      <li><input type="text" class="inputFields" name="post_title" placeholder="Titre" required/></li>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script>
 
-      <!-- - DATE DE SORTIE - -->
-      <li><input type="text" class="inputFields" name="post_date" placeholder="Date de sortie" required/></li>
+  function getData (){
+    var url = $("input#getData_url").val();
+    url = url.slice(27,36);
+    console.log(url);
+    const dataForm = fetch('https://api.themoviedb.org/3/movie/' + url + '?api_key=1b523a22be6bbcb7b260693e379e6889')
+      .then (result => result.json())
+      .then (data => {
+        const data_title =  data.original_title;
+        const data_date =  data.release_date;
+        const data_duration =  data.runtime+" minutes";
+        const data_synopsis =  data.overview;
+        const data_poster =  "https://image.tmdb.org/t/p/w500"+data.poster_path;
+        $("input#title").val(data_title);
+        $("input#date").val(data_date);
+        $("input#duration").val(data_duration);
+        $("input#poster").val(data_poster);
+        $('#synopsis').val(data_synopsis);
+    });
 
-      <!-- - REALISATEUR - -->
-      <li><input type="text" class="inputFields" name="post_real" placeholder="Réalisateur" required/></li>
+    const data2Form = fetch('https://api.themoviedb.org/3/movie/'+ url + '/videos?api_key=1b523a22be6bbcb7b260693e379e6889&language=fr-FR')
+      .then (result => result.json())
+      .then (data => {
+        if (data.results[0].type == "Trailer"){
+          const data_video = "youtube.com/watch?v="+data.results[0].key;
+          $("input#ytb").val(data_video);
+        } else if (data.results[1].type == "Trailer"){
+            const data_video = "youtube.com/watch?v="+data.results[1].key;
+            $("input#ytb").val(data_video);
+        } else if (data.results[1].type == "Trailer"){
+            const data_video = "youtube.com/watch?v="+data.results[1].key;
+            $("input#ytb").val(data_video);}
+      });
 
-      <!-- - GENRE - -->
-      <li style="column-width: 12rem">
-        <label class="container">Animation
-            <input type="checkbox" name="post_kind[]" value="animation">
-            <span class="checkmark"></span>
-        </label>
-        <label class="container">Biopic
-          <input type="checkbox" name="post_kind[]" value="biopic">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Comédie
-          <input type="checkbox" name="post_kind[]" value="comedie">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Documentaire
-          <input type="checkbox" name="post_kind[]" value="documentaire">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Drame
-          <input type="checkbox" name="post_kind[]" value="drame">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Fantastique / S-F
-          <input type="checkbox" name="post_kind[]" value="fantastique">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Horreur
-          <input type="checkbox" name="post_kind[]" value="horreur">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Historique
-          <input type="checkbox" name="post_kind[]" value="historique">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Policier / Thriller
-          <input type="checkbox" name="post_kind[]" value="policier">
-          <span class="checkmark"></span>
-        </label>
-        <label class="container">Aventure
-          <input type="checkbox" name="post_kind[]" value="aventure">
-          <span class="checkmark"></span>
-        </label>
+      const data3Form = fetch ('https://api.themoviedb.org/3/movie/' + url + '/credits?api_key=1b523a22be6bbcb7b260693e379e6889')
+        .then (result => result.json())
+        .then (data => {
+            console.log(data);
+            var crew_length = data.crew.length;
+            var data_director;
+            for (var i = 0; i < crew_length; i++){
+              if (data.crew[i].job == "Director"){
+                data_director = data.crew[i].name;
+              } else {}
+            }
+            $("input#real").val(data_director);
+            const data_actors = [data.cast[0].name, data.cast[1].name, data.cast[2].name];
+            $("input#actors").val(data_actors[0]+", "+data_actors[1]+", "+data_actors[2]);
+        })
 
-      </li>
+  }  </script>
+</body>
 
-      <!-- - DUREE - -->
-      <li><input type="text" class="inputFields" name="post_duration" placeholder="Durée" required/></li>
-
-      <!-- - LANGUES - -->
-      <li><input type="text" class="inputFields" name="post_language" placeholder="Langue(s)" required/></li>
-
-      <!-- - SUBS - -->
-      <li><input type="text" class="inputFields" name="post_subs" placeholder="Sous-titre(s)" required/></li>
-
-      <!-- - QUALITE - -->
-      <li style="column-width: 7rem; padding-left: 2.5rem; padding-top: 1rem; padding-bottom: 1rem;" >
-        <label class="containerQ">- 720p
-            <input type="radio" name="post_quality" value="- 720p">
-            <span class="checkmarkQ"></span>
-        </label>
-        <label class="containerQ"> 720p
-            <input type="radio" name="post_quality" value="720p">
-            <span class="checkmarkQ"></span>
-        </label>
-        <label class="containerQ">1080p
-          <input type="radio" name="post_quality" value="1080p">
-          <span class="checkmarkQ"></span>
-        </label>
-      </li>
-
-			<!-- - IMDb - -->
-      <li><input type="url" class="inputFields" name="post_imdb" placeholder="imdb link" required/></li>
-
-			<!-- - Allocine - -->
-			<li><input type="url" class="inputFields" name="post_allocine" placeholder="allocine link" required/></li>
-
-			<!-- - Rottent Tomatoes - -->
-			<li><input type="url" class="inputFields" name="post_rt" placeholder="rotten tomatoes link" required/></li>
-
-      <!-- - SYNOPSIS - -->
-      <li><input type="text" class="inputFields" name="post_synopsis" placeholder="Synopsis" required/></li>
-
-      <!-- - BANDE-ANNONCE - -->
-      <li><input type="text" class="inputFields" name="post_ba" placeholder="Lien de la Bande-Annonce" required/></li>
-
-      <!-- - MINIATURE - -->
-			<input type="hidden" name="MAX_FILE_SIZE" value="4194304" />
-      <li><input type="file" name="post_pic" /></li>
-
-      <!-- - SUBMIT - -->
-      <li id="center-btn">
-        <input type="submit" id="join-btn" value="Submit">
-      </li>
-    </ul>
-  </form>
-
-</div></body></html>
+</html>
--- index.php
+++ index.php
@@ -1,53 +1,22 @@
-<?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
-
-	require_once('includes/class-query.php');
-
-	if ( !empty ( $_GET ) ) {
-		if ( !empty ( $_GET['p'] ) ) {
-			$post = $_GET['p'];
-		}
-
-		if ( !empty ( $_GET['cat'] ) ) {
-			$cat = $_GET['cat'];
-		}
-	}
-
-	if ( empty ( $post ) && empty ( $cat ) ) {
-		$posts_array = $query->all_posts();
-	} elseif ( !empty ( $post ) ) {
-		$posts_array = $query->post($post);
-	} elseif ( !empty ( $cat ) ) {
-		echo 'cat';
-	}
-?>
 <html>
-	<head>
-		<title>CMS</title>
-		<meta charset="utf-8" />
-		<link type="text/css" rel="stylesheet" href="./assets/css/main.css"/>
-	</head>
+<head>
+  <title>Accueil</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+  <?php
+  require_once('config.php');
+  $conn = mysqli_connect(host, user,pass, db);
 
-	<body>
+  $sql = "SELECT * FROM articles";
+  $result = mysqli_query($conn, $sql);
 
-	<div>
-	<nav id="sidebar">
-	  <ul>
-	    <a href="./index.php"><i class="fa fa-film icon" style="color: #FFFFFF;" aria-hidden="true"></i></a>
-	    <i class="fa fa-television icon" style="color: #FFFFFF;" aria-hidden="true"></i>
-	  </ul></nav>
-	<div class="highbar">
-	  <h4>KELOU MEDIACENTER - DEV EDIT</h4>
-		<a href="./admin/add.php"><button class="Add-button">Add a movie</button></a>
-	</div>
-	<div class="divider">
-
-		<?php foreach ( $posts_array as $post ) : ?>
-			<div class="article" style="background-image: url(<?php echo $post->post_pic; ?>); background-size: cover; background-repeat: no-repeat;">
-					<a href="./article.php?p=<?php echo $post->ID; ?>">
-					<p class="name"><?php echo $post->post_title ?></p></a></div>
-		<?php endforeach; ?>
-	</div>
-	</body>
+  if (mysqli_num_rows($result) > 0){
+    while ($data = mysqli_fetch_assoc($result)){
+      echo '<a href=article.php?id='.$data['ID'].'>'.$data['title'].'</a>'."<br />";
+      echo $data['date']."<br /><br />";
+    }
+  }
+  ?>
+</body>
 </html>
