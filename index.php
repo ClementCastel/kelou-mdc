8e04d2f6c767f645b0c18b18e39d093f96c148a7
Clément Castel <clement.castel75@gmail.com>
2018-07-10 17:22:42 +0200

    movie.php integration
    + movie.php integration
    + clickable logo on index.php

--- index.php
+++ index.php
@@ -34,12 +34,13 @@
         if (mysqli_num_rows($result) > 0){
             while ($data = mysqli_fetch_assoc($result)){ ?>
 
+            <a href="movie.php?id=<?php echo $data['ID'];?>">
             <div class="p-movie" style="width:180px;height:335px;">
                 <div class="movie" style="width:180px;height:270px;background-image:url(&quot; <?php echo $data['poster']?> &quot;);"></div>
                 <div style="height:65px;background-color:#ffffff;align-content:center;width:180px;line-height:65px;text-align:center;">
                     <p style="font-family:'Barlow Condensed', sans-serif;font-size:20px;color:rgb(0,0,0);font-weight:normal;font-style:normal;width:100%;vertical-align:middle;display:inline-block;line-height:1.2;margin-bottom:0;margin-top:-5px;"><?php echo $data['title']?></p>
                 </div>
-            </div>
+            </div></a>
 
       <?php }
       }
